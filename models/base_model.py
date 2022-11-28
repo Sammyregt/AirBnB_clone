3524b1bf395af517c6150b665de8cfc8409a1d5c
Sammyregt <adeoyesamuel100@gmail.com>
2022-11-28 05:41:37 +0000

    update: files

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -18,6 +19,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
+
         else:
             f = "%Y-%m-%dT%H:%M:%S.%f"
             for keys, value in kwargs.items():
@@ -39,6 +42,7 @@ class BaseModel:
             Update last update time with the current time
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
